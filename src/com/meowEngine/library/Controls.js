a05d404edd961996f2a1f61afb2fda54f4eedc18
Tapsi <blackcat.myako@gmail.com>
2011-07-11 21:49:33 +0000

    Fix repo #3
    --HG--
    extra : convert_revision : svn%3Ad046594a-c978-11de-a70d-3b9dd4243769/trunk%40345

---
+++ src/com/meowEngine/library/Controls.js
@@ -0,0 +1,134 @@
+(function(){
+
+    // MEOW CONTROL MODULE
+	// ====================
+	//
+	// LICENSE: MEOW LICENSE (SEE LICENSE FILE)
+	// SINCE: 10.07.2011
+	//======================================================================
+
+    
+	var _downKeys = {};
+	var _actions = {};
+
+	var _on = 1;
+	var _off = 0;
+
+    /** @namespace */
+    meowEngine.input = {};
+
+    /**
+     * Registeres a key down event in the meowEngine control system.
+     *
+     * @param {Number} keyID key identical number
+     */
+	meowEngine.input.keyDown = function( keyID ){
+		_downKeys[keyID] = _on;
+	};
+
+    /**
+     * Registeres a key up event in the meowEngine control system.
+     *
+     * @param {Number} keyID key identical number
+     */
+	meowEngine.input.keyUp = function( keyID ){
+		// dont delete keys, because normally they
+		// will be invoked later again
+		_downKeys[keyID] = _off;
+	};
+
+    /**
+     * Binds a key with an action.
+     *
+     * @param {Number} keyID key identical number
+     * @param {String} action action name
+     */
+	meowEngine.input.bindKey = function( keyID, action ){
+
+		if( typeof action !== 'string' ){
+			throw "Input, bindKey has an illegal argument";
+		}
+
+		if( !_actions[action] ){
+			_actions = new Array();
+		}
+		else if( _actions[action].indexOf(keyID) != -1 ){
+			return;
+		}
+
+		_actions[action].push(keyID);
+	};
+
+    /**
+     * Unbinds a key from an action.
+     *
+     * @param {Number} keyID key identical number
+     * @param {String} action action name
+     */
+	meowEngine.input.unbindKey = function( keyID, action ){
+
+		assert.isString(action);
+		var array = _actions[action];
+
+		if( !array ){
+			throw "action is undefined or empty, both normally can't be!";
+		}
+
+		var i = array.indexOf(keyID);
+		if( i != -1 ){
+			array.splice( i , 1 );
+		}
+
+		// delte array if empty
+		if( array.length == 0 ){
+			delete _actions[action];
+		}
+		array = null;
+	};
+
+    /**
+     * Checks the status of a key ID.
+     *
+     * @return true, if the key is down, else false
+     */
+    meowEngine.input.key = function( keyID ){
+        
+        return _downKeys[ keyID ] == _on;
+	};
+
+    /**
+     * Chechks the status of the given action.
+     *
+     * @return true, if the action is active, else false
+     */
+	meowEngine.input.action = function( action ){
+
+		if( typeof action !== 'string' ){
+			throw "Input, isActive has an illegal argument";
+		}
+
+		var array = _actions[action];
+
+		if( !array ){
+			throw "action is undefined or empty, both normally can't be!";
+		}
+
+		var i,e = array.length;
+		for( i = 0; i < e ; i++ ){
+
+			// if one is triggered, action is true
+			if( _downKeys[ array[i] ] ){
+				return true;
+			}
+		}
+
+		return false;
+	};
+
+    // shortcuts
+    meowEngine.signals.onSignal( meowEngine.MEOW_SHORTCUTS , function(){
+
+       $inputAction = meowEngine.input.action;
+    });
+
+})();
