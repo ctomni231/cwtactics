2830d284fe05a12ec2dba67c39a5ff5145aebbab
Tapsi <blackcat.myako@gmail.com>
2012-07-22 15:46:00 +0200

    move logic

---
+++ mod/weapons.js
@@ -0,0 +1,19 @@
+var _tmp_ = {
+  "sheets" : [
+
+    {
+      "ID"            : "WP_MG",
+      "useAmmo"       : 0,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+  ]
+};
+
+for( var i = 0; i<_tmp_.sheets.length; i++ ){
+  app.module("db").parse( _tmp_.sheets[i], app.module("db").types.WEAPONS );
+}
