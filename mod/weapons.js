9979fdee445b9259a51559d24c20a6d104904e9a
ctomni231 <ctomni231@gmail.com>
2012-09-27 15:17:04 -0700

    Issue #17 Commit
    Fixed issue 17 adding all the weapons needed for the AWDS modification.
    Might need to refactor it since many units use the same names for
    weapons.

--- mod/weapons.js
+++ mod/weapons.js
@@ -10,4 +10,125 @@ cwt.mod.awds.weapons = [
         "*"         : 0
       }
     }
+
+    {
+      "ID"            : "WP_BZ",
+      "useAmmo"       : 3,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_VL",
+      "useAmmo"       : 8,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_CA",
+      "useAmmo"       : 6,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_BO",
+      "useAmmo"       : 6,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_RC",
+      "useAmmo"       : 4,
+      "minRange"      : 3,
+      "maxRange"      : 5,
+      "fireType"      : "INDIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_TR",
+      "useAmmo"       : 6,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_MO",
+      "useAmmo"       : 2,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_SH",
+      "useAmmo"       : 5,
+      "minRange"      : 1,
+      "maxRange"      : 3,
+      "fireType"      : "INDIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_FL",
+      "useAmmo"       : 3,
+      "minRange"      : 2,
+      "maxRange"      : 5,
+      "fireType"      : "INDIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_MN",
+      "useAmmo"       : 1,
+      "minRange"      : 1,
+      "maxRange"      : 1,
+      "fireType"      : "DIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
+
+    {
+      "ID"            : "WP_MI",
+      "useAmmo"       : 4,
+      "minRange"      : 3,
+      "maxRange"      : 6,
+      "fireType"      : "INDIRECT",
+      "damages"        :{
+        "*"         : 0
+      }
+    }
 ];
