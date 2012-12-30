613ac30c45b2dea086307b18324749a931f2a474
ctomni231 <ctomni231@gmail.com>
2012-12-30 03:17:09 -0800

    JSON Parser (Roman Numeral)
    JSON to XML conversion finally done without errors. Numerals are not
    allowed in XML Tags, so I made a converter that translates all numbers
    to Roman Numeral format to handle any number found in a tag. Check out
    filler.xml and fillernew.xml to see the difference between the XML
    sheets. Now all documents in JSON can be converted seamlessly to XML.
    
    Added MaraSargon to the list of credits for his hard work on the forums.
    Thanks!

--- src/com/cwt/system/jslix/tools/JSON_Parser.java
+++ src/com/cwt/system/jslix/tools/JSON_Parser.java
@@ -38,19 +38,16 @@ public class JSON_Parser extends XML_Parser{
 			while(scanner.hasNext())
 				script = script + scanner.nextLine() + "\n";
 			parser = new JSONObject( script );
-			System.out.println(XML.toString( parser , 
-					filename.substring(filename.lastIndexOf("/")+1, filename.indexOf(".")) ));
-			//parseData(XML.toString( parser ));
+			
+			//Changes the JSON into an XML file in one pass
+			parseData(formatXML(XML.toString( parser , 
+				filename.substring(filename.lastIndexOf("/")+1, filename.indexOf(".")))));
 			
 		} catch (FileNotFoundException e) {
 			System.err.println(e);
 		} catch (JSONException e) {
 			System.err.println(e);
 		}
-	}
-	
-	public void parseDocument(JSONArray array){
-		
 	}
 	
 	public void get(){
@@ -72,15 +69,93 @@ public class JSON_Parser extends XML_Parser{
 		return script;
 	}
 	
+	/**
+	 * This class takes a solid String of unformatted XML code and converts it
+	 * into proper formatted XML code readable by the XML_Parser class.
+	 * @param data A solid block of unreadable XML code from JSONObject
+	 * @return A properly formatted XML code
+	 */
+	private String formatXML( String data ){
+		XML_Writer writer = new XML_Writer();
+		String temp = "";
+		while(data.length() > 0){
+			temp = data.substring(0, data.indexOf(">"));
+			if(temp.matches("</.*"))
+				writer.endXMLTag();
+			else if(temp.charAt(0) == '<'){	
+				temp = temp.substring(1);
+				writer.addXMLTag(temp.matches("\\d.*") ? convertRomanNumeral(temp) : temp);
+			}else
+				writer.addAttribute("data", temp.substring(0, data.indexOf("<")), true);
+			data = data.substring(data.indexOf(">")+1);
+		}
+		return writer.getRawXML();
+	}
+	
+	/**
+	 * This function takes normal numbers and converts them into Roman numerals. It's main
+	 * purpose is to help keep the integrity of the XML data to stay well formed. 
+	 * @param data Numerical data in String format
+	 * @return The Roman numeral value of the data
+	 */
+	private String convertRomanNumeral( String intData ){
+		int num = Integer.valueOf(intData);
+		intData = "";//Reused this as temporary.
+		while(num > 0){
+			if(num >= 1000-100){
+				if(num < 1000){
+					intData = intData + "C";
+					num += 100;
+				}
+				intData = intData + "M";
+				num -= 1000;
+			}else if(num >= 500-100){
+				if(num < 500){
+					intData = intData + "C";
+					num += 100;
+				}
+				intData = intData + "D";
+				num -= 500;
+			}else if(num >= 100-10){
+				if(num < 100){
+					intData = intData + "X";
+					num += 10;
+				}
+				intData = intData + "C";
+				num -= 100;
+			}else if(num >= 50-10){
+				if(num < 50){
+					intData = intData + "X";
+					num += 10;
+				}
+				intData = intData + "L";
+				num -= 50;
+			}else if(num >= 10-1){
+				if(num < 10){
+					intData = intData + "I";
+					num += 1;
+				}
+				intData = intData + "X";
+				num -= 10;
+			}else if(num >= 5-1){
+				if(num < 5){
+					intData = intData + "I";
+					num += 1;
+				}
+				intData = intData + "V";
+				num -= 5;
+			}else{
+				intData = intData + "I";
+				num -= 1;
+			}
+		}
+		return intData;
+	}
+	
 	public static void main(String[] args){
 		JSON_Parser parse = new JSON_Parser();
 		parse.parse("map/test.json");
 		//parse.get();
 		//System.out.println( parse.getScript() );	
-	}
-	
-	private String formatXML( String data ){
-		XML_Writer writer = new XML_Writer();
-		return data;
 	}
 }
