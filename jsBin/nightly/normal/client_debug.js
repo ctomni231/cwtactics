e3a630bad9b5c594448ec1dcb8132a6e86146ad9
Tapsi <blackcat.myako@gmail.com>
2013-01-02 16:56:40 +0100

    Bugfix buildUnit

--- jsBin/nightly/normal/client_debug.js
+++ jsBin/nightly/normal/client_debug.js
@@ -1855,6 +1855,26 @@ view.registerCommandHook({
 });
 view.registerCommandHook({
 
+  key: "buildUnit",
+
+  prepare: function( data ){
+
+    var x = data.getSourceX();
+    var y = data.getSourceY();
+    var unit = model.unitPosMap[x][y];
+    controller.updateUnitStats( unit );
+  },
+
+  render: function(){},
+  update: function(){},
+
+  isDone: function(){
+    return true;
+  }
+
+});
+view.registerCommandHook({
+
   key: "captureProperty",
 
   prepare: function( data ){
