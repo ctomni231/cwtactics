fc89814dddf56157070215f47246f143cef23ef4
BlackCat <blackcat.myako@gmail.com>
2013-01-07 19:37:13 +0100

    Update content/news.js

--- content/news.js
+++ content/news.js
@@ -1,5 +1,6 @@
 var PAGE_NEWS = {
   "news":[
+    { date:"01.07.2013", content:"In future we will release sub milestones only if we fix critical bug from the main milestone. Bugs that aren't critical will be fixed always in the next major milestone." },
     { date:"01.07.2013", content:"Milestone 2.2 Update" },
     { date:"01.07.2013", content:"Milestone 2.1 Update" },
     { date:"01.07.2013", content:"Added compatibility map" },
