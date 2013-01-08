845737e805e88e45c71be95b1d65ca161e7f0eed
BlackCat <blackcat.myako@gmail.com>
2013-01-08 10:36:47 +0100

    Update content/compatibility.js
    added main environments

--- content/compatibility.js
+++ content/compatibility.js
@@ -1,18 +1,29 @@
 var PAGE_COMPA = {
   "compabilities":[
+    
     { name:"Mobile Safari on iOS6", level:"fine", notes:"" },
     { name:"Web View on iOS6", level:"fine", notes:"" },
+    
     { name:"Chrome on Android 4.0", level:"fine", notes:"May have slow move animations" },
     { name:"Chrome on Android 4.1", level:"fine", notes:"May have slow move animations" },
     { name:"Chrome 23 (MacOSX)", level:"fine", notes:"" },
     { name:"Chromium 20 (Linux)", level:"fine", notes:"" },
-    { name:"Safari 5.1", level:"unknown", notes:"" },
-    { name:"Safari 6", level:"unknown", notes:"" },
-    { name:"Firefox 17 (MacOSX)", level:"problem", notes:"Object color issues" },
-    { name:"Firefox 18 (Linux)",  level:"problem", notes:"Speed issues" },
-    { name:"Firefox 19 (MacOSX)", level:"problem", notes:"Object color issues" },
+    { name:"Chrome 2x (Windows)", level:"unknown", notes:"" },
+    
+    { name:"Safari 5.1 (MacOSX)", level:"unknown", notes:"" },
+    { name:"Safari 6 (MacOSX)", level:"unknown", notes:"" },
+    { name:"Safari 5.1 (Windows)", level:"unknown", notes:"" },
+    { name:"Safari 6 (Windows)", level:"unknown", notes:"" },
+    
+    { name:"Firefox 17 (MacOSX)", level:"problem", notes:"Color issues" },
+    { name:"Firefox 18 (Linux)",  level:"problem", notes:"Speed issues and color issues" },
+    { name:"Firefox 19 (Windows)", level:"unknown", notes:"unknown" },
+    
     { name:"Internet Explorer 9", level:"error", notes:"Fails to start" },
     { name:"Internet Explorer 10", level:"unknown", notes:"" },
-    { name:"Opera 12", level:"unknown", notes:"" }
+    
+    { name:"Opera 12 (MacOSX)" , level:"unknown", notes:"" },
+    { name:"Opera 12 (Linux)"  , level:"unknown", notes:"" },
+    { name:"Opera 12 (Windows)", level:"unknown", notes:"" }
   ]
 };
