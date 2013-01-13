6180eeca8a0a9f373923e41557c6288c9e46d20e
BlackCat <blackcat.myako@gmail.com>
2013-01-13 18:29:43 +0100

    Update content/news.js

--- content/news.js
+++ content/news.js
@@ -1,11 +1,12 @@
 var PAGE_NEWS = {
   "news":[
+    { date:"01.13.2013", content:"The complete project roadmap is now available in the navigation bar" },
     { date:"01.12.2013", content:"Updated the <span style='font-style:italic;'>Milestone 3</span> roadmap" },
     { date:"01.11.2013", content:"Uploaded a first version of the <span style='font-style:italic;'>Milestone 3</span> roadmap" },
     { date:"01.07.2013", content:"In future we will release sub milestones only if we fix critical bugs from the active milestone. Bugs that aren't critical will be fixed always in the next major milestone." },
-    { date:"01.07.2013", content:"Milestone 2.2 Update" },
-    { date:"01.07.2013", content:"Milestone 2.1 Update" },
-    { date:"01.07.2013", content:"Added compatibility map" },
-    { date:"01.04.2013", content:"Updated Github webpage design" }
+    { date:"01.07.2013", content:"Milestone 2.2 Update" } //,
+    //{ date:"01.07.2013", content:"Milestone 2.1 Update" },
+    //{ date:"01.07.2013", content:"Added compatibility map" },
+    //{ date:"01.04.2013", content:"Updated Github webpage design" }
   ]
 };
