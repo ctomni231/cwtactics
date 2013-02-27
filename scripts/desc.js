ec055198a359f5aa5f73e27b18976febd30eca83
Tapsi <blackcat.myako@gmail.com>
2013-02-27 23:11:16 +0100

    M 2.61 #2

--- scripts/desc.js
+++ scripts/desc.js
@@ -1,27 +1,63 @@
-var PAGE_NEWS_DESC = {
-  "table tr": {
-    "newsEntry <- news": {
-      "td.date": "#{newsEntry.date}",
-      "td.content": "#{newsEntry.content}"
+/* ***************************************************************** */
+
+PAGE_DESC.linkSection = {
+  
+  "li":{
+    "entry <- links":{
+      
+      "a":"#{entry.name}",
+      
+      "a@href":"#{entry.link}"
     }
   }
+  
 };
 
-var PAGE_COMPA_DESC = {
-  "table tr": {
-    "entry <- compabilities": {
-      "td.name": "#{entry.name}",
-      "td.lvWin": "#{entry.levelWin}",
-      "td.lvLin": "#{entry.levelLin}",
-      "td.lvMac": "#{entry.levelMac}",
-      "td.lvDrd": "#{entry.levelDrd}",
-      "td.lvIOS": "#{entry.levelIOS}",
-      "td.notes": "#{entry.notes}"
+/* ***************************************************************** */
+
+PAGE_DESC.legalSection = {
+  
+  "div.infoBox":{
+    "entry <- legals": {
+
+      "div.infoBoxHeader": function(args) {
+        var item = args.item;
+        return [
+          item.header,
+          "<span>",
+          item.version,
+          "</span><span class='role'>",
+          item.subHeaderT,
+          "<br/>",
+          item.subHeaderB,
+          "</span>"
+        ].join("");
+      },
+      
+      "p.text":{
+        "part <- entry.text": {
+          ".":"#{part}"
+        }
+      },
+            
+      "a@href": "#{entry.link}",
+      "a@style": function( args ){
+        var link = args.item.link;
+        if( typeof link !== 'undefined' && link !== null && link.length > 0 ){
+          return "";
+        }
+        return "display:none";
+      }
+      
     }
   }
+  
 };
 
-var PAGE_MILESTONES_DESC = {
+/* ***************************************************************** */
+
+PAGE_DESC.milestoneSection = {
+  
   "div.infoBox":{
     "entry <- milestones": {
 
@@ -38,24 +74,27 @@ var PAGE_MILESTONES_DESC = {
           "</span>"
         ].join("");
       },
-
-      "p.firstText": "#{entry.pre}",
-
+      
       "p.imgBox": function(args) {
         if( typeof args.item.img !== 'undefined' && args.item.img !== null ){
           return "<img src='"+args.item.img+"'>";
         }
         else return "";
       },
-
-      "p.lastText": "#{entry.post}",
-
-      "a@href": "#{entry.link}"
-      ,
+    
+      "p.text":{
+        "part <- entry.text": {
+          ".":"#{part}"
+        }
+      },
       
-      "a": "#{entry.linkCaption}"
-      ,
+      "li.changelog":{
+        "log <- entry.changelog": {
+          ".":"#{log}"
+        }
+      },
       
+      "a@href": "#{entry.link}",
       "a@style": function( args ){
         var link = args.item.link;
         if( typeof link !== 'undefined' && link !== null && link.length > 0 ){
@@ -63,16 +102,41 @@ var PAGE_MILESTONES_DESC = {
         }
         return "display:none";
       }
+      
     }
   }
+  
 };
 
-var PAGE_NAVBAR_DESC = {
+/* ***************************************************************** */
+
+PAGE_DESC.navBar = {
   "li":{
-    "entry <- links":{
-      "a":"entry.name",
-      "a@id":"entry.id",
-      "a@href":"entry.link"
+    "entry <- navBar":{
+      
+      "a":"#{entry.name}",
+      
+      "a@section":"#{entry.section}",
+      
+      "a@href":function( args ){
+        var target = args.item.section;
+        var link = args.item.link;
+        
+        if( link === true ){
+          return target;
+        }
+        return "";
+      },
+      
+      "a@target":function( args ){
+        var target = args.item.section;
+        var link = args.item.link;
+        
+        if( link === true && target.substr(0,6) !== "mailto" ){
+          return "_blank";
+        }
+        return "_self";
+      }
     }
   }
 };
--- scripts/stuff.js
+++ scripts/stuff.js
@@ -1,73 +1,50 @@
-function PAGE_CONVERT_TAG_CLASSES(){
-  var now = new Date();
+var PAGE_DESC = {};
+var PAGE_DATA = {};
+var PAGE_PROG = {};
 
-  // DATES
-  var elements = document.getElementsByClassName("dateDiff");
-  for( var i = 0,e = elements.length; i<e; i++ ){
-    var dateVal = Date.parse( elements[i].innerHTML );
-    var diff = now - dateVal;
-    var daysDiff = Math.floor(diff/1000/60/60/24);
+/* ***************************** SECTION ***************************** */
 
-    var value;
-    if( daysDiff === 0 ) value = "Today";
-    else if( daysDiff === 1 ) value = "Yesterday";
-    else if( daysDiff < 7 ) value = "This Week";
-    else if( daysDiff < 30 ) value = "Last Weeks";
-    else if( daysDiff < 60 ) value = "Last Month";
-    else if( daysDiff < 365 ) value = "Last Months";
-    else if( daysDiff >= 365 ) value = "Last Year";
-    else value = "Wheeew... old!";
+PAGE_PROG.oldSection_ = null;
+PAGE_PROG.newSection_ = null;
 
-    elements[i].innerHTML = value;
+PAGE_PROG.fadeSectionIn = function(){
+  $( "#"+PAGE_PROG.newSection_ ).fadeIn(500);
+  PAGE_PROG.newSection_ = null;
+};
+
+PAGE_PROG.openSection = function( el, section ){
+  if( el.href !== "" ) return true;
+  
+  if( PAGE_PROG.oldSection_ !== null ){
+    PAGE_PROG.newSection_ = section;
+    $("#"+PAGE_PROG.oldSection_).fadeOut(500,PAGE_PROG.fadeSectionIn);
   }
-
-  // COMPA MAP
-  elements = document.getElementsByClassName("withStatusIcon");
-  for( var i = 0,e = elements.length; i<e; i++ ){
-    var value = elements[i].innerHTML;
-    var title = "";
-    
-    if( value === "" ) continue;
-
-
-    if( value === "fine" ) title = "Runs fine";
-    else if( value === "problem" ) title = "Some faults may happen";
-    else if( value === "error" ) title = "Does not start";
-    else if( value === "unknown" ) title = "Not tested yet";
-    else if( value === "none" ) title = "Not available on platform";
-    else if( value === "ico_win" ) title = "Microsoft Windows";
-    else if( value === "ico_mac" ) title = "Apple Mac Osx";
-    else if( value === "ico_lin" ) title = "Linux";
-    else if( value === "ico_drd" ) title = "Android 4";
-    else if( value === "ico_ios" ) title = "Apple iOS 6";
-    else if( value === "Chrome" ) title = "Google Chrome";
-    else if( value === "Firefox" ) title = "Firefox";
-    else if( value === "Safari" ) title = "Apple Safari";
-    else if( value === "InternetExplorer" ) title = "Internet Explorer";
-    else title = "Unknown";
-
-
-    if( value === "fine" ) value = "sign_tick.png";
-    else if( value === "problem" ) value = "sign_warning.png";
-    else if( value === "error" ) value = "sign_cacel.png";
-    else if( value === "unknown" ) value = "sign_question.png";
-    else if( value === "none" ) value = "sign_deny.png";
-
-    else if( value === "ico_win" ) value = "ico_win.png";
-    else if( value === "ico_mac" ) value = "ico_mac.png";
-    else if( value === "ico_lin" ) value = "ico_lin.png";
-    else if( value === "ico_drd" ) value = "ico_drd.png";
-    else if( value === "ico_ios" ) value = "ico_ios.png";
-
-    else if( value === "Chrome" ) value = "ico_ch.png";
-    else if( value === "Firefox" ) value = "ico_ff.png";
-    else if( value === "Safari" ) value = "ico_sf.png";
-    else if( value === "InternetExplorer" ) value = "ico_ie.png";
-    else value = "sign_question.png";
-
-
-    value = "<img title='"+title+"' src='icons/"+value+"'></img>";
-
-    elements[i].innerHTML = value;
+  else{
+    PAGE_PROG.newSection_ = section;
+    PAGE_PROG.fadeSectionIn();
   }
-}
+  
+  PAGE_PROG.oldSection_ = section;
+  return false;
+};
+
+
+/* ***************************** ON READY ***************************** */
+
+$(document).ready(function(){
+
+  // RENDER TEMPLATES
+  $("#navBar").render( PAGE_DATA, PAGE_DESC.navBar);
+  $("#sectionRelease").render( PAGE_DATA, PAGE_DESC.milestoneSection );
+  $("#sectionLegal").render( PAGE_DATA, PAGE_DESC.legalSection );
+  $("#sectionLinks").render( PAGE_DATA, PAGE_DESC.linkSection );
+      
+  // CONVERT RSS DATES TO HUMAN READABLE TIME
+  $(".rss-date").each(function(i,element){
+    element.innerHTML = moment( element.innerHTML, "YYYY-MM-DD HH:mm:ss.SSS-Z,ZZ").fromNow();;
+  });
+      
+  $("#playLatestButton").attr("href", PAGE_DATA.latestMilestone.link );
+  
+  PAGE_PROG.openSection( { href:"" }, "sectionMain" ); 
+});
