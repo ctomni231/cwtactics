a9f73cd63300bb254f84bbb7c6285320e9e29b98
Cesar Ramirez <ramirez@cesarandres.com>
2013-06-02 22:35:57 +0000

    Prototype almost ready

--- server/api.pl
+++ server/api.pl
@@ -5,6 +5,7 @@
 # retrieve - get the list of actions for a game session
 	
 use CGI;
+use DBI;
 
 sub rnd_str
 {
@@ -21,27 +22,79 @@ sub rnd_str
 
 sub create
 {
-
+    my $session = rnd_str(24);
+    my $dbh = DBI->connect("dbi:SQLite:dbname=test.db", "", "", { RaiseError => 1}) or die $DBI::errstr;
+    $dbh->do("INSERT INTO Session VALUES('$session','$2')");
+    open (MYFILE, ">$session") || die "Cannot Open File";
+    $dbh->disconnect();
+    return $session;
 }
 
-sub delete
+sub dlete
 {
-
+    my $dbh = DBI->connect("dbi:SQLite:dbname=test.db", { RaiseError => 1 }) or die $DBI::errstr;
+    my $sth = $dbh->prepare( "SELECT * FROM Session WHERE uuiid=$1 AND token=$2");  
+    $sth->execute();
+    $sth->fetchrow();
+    $dbh->disconnect();
+    if ( $sth->rows() == 1 )
+    {
+        unlink($1);
+        return "Success";
+    }
+    else 
+    { 
+       return "Fail";
+    }
 }
 
 sub append
 {
-
+    my $dbh = DBI->connect("dbi:SQLite:dbname=test.db", { RaiseError => 1 }) or die $DBI::err;
+    my $sth = $dbh->prepare( "SELECT * FROM Session WHERE uuid=$1 AND token=$2");
+    $sth->execute();
+    $sth->fetchrow();
+    $dbh->disconnect();
+    if ( $sth->rows() == 1 )
+    {
+        my $message = $3;
+        open(DAT,">>$2") || die("Cannot Open File");
+        print DAT $message;
+        return "Success";
+    }
+    else
+    {
+       return "Fail";
+    } 
 }
 
 sub retrieve
 {
-
+    my $dbh = DBI->connect("dbi:SQLite:dbname=test.db", { RaiseError => 1 }) or die $DBI::err;
+    my $sth = $dbh->prepare( "SELECT * FROM Session WHERE uuid=$1 AND token=$2");
+    $sth->execute();
+    $sth->fetchrow();
+    $dbh->disconnect();
+    if ( $sth->rows() == 1 )
+    {
+        open (DAT, $2);
+        while (<MYFILE>) {
+ 	    chomp;
+            print "$_\n";
+        }
+        close (MYFILE);
+        return "Success";
+    }
+    else 
+    { 
+        return "Fail";
+    }
 }
 
 $cgi = new CGI;
 
-for $key ( $cgi->param() ) {
+for $key ( $cgi->param() )
+{
     $input{$key} = $cgi->param($key);
 }
 
@@ -50,35 +103,47 @@ print $cgi->header('text/html');
 action;
 param;
 token;
+message;
 
-for $key ( keys %input ) {
+for $key ( keys %input )
+{
     if ( $key == "action" )
     {
         $action =  $input{$key};
     }
-    else if ( $key == "param")
+    elsif ( $key == "param")
     {
         $param =  $input{$key};
     }
-    else if ( $key == "token")
+    elsif ( $key == "token")
     {
         $token =  $input{$key};
+    }
+    elsif ( $key == "message")
+    {
+        $message =  $input{$key};
     }
 }
 
 if ( $action == "create" )
 {
-    
+    return create($token);
 } 
-else if ( $action == "delete" )
+elsif ( $action == "delete" )
 { 
-
+    return dlete($param, $token);
 } 
-else if ( $action == "append" )
+elsif ( $action == "append" )
 { 
-
+    return append($param, $token, $message);
 }
-else if ( $action == "retrive" )
+elsif ( $action == "retrive" )
 { 
-
+    return retrieve($param, $token);
+}
+else
+{
+    my $dbh = DBI->connect("dbi:SQLite:dbname=test.db", "", "", { RaiseError => 1}) or die $DBI::errstr;
+    $dbh->do("CREATE TABLE Session(uuid TEXT, password TEXT)");
+    $dbh->disconnect();   
 }
