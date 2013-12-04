fffe55cbe39d2b25fb72ed7f2b2ba84885b87d97
BlackCat <blackcat.myako@gmail.com>
2013-12-04 08:43:40 +0100

    Update error.js

--- srcWebClient/controller/error.js
+++ srcWebClient/controller/error.js
@@ -1,8 +1,8 @@
 util.scoped(function(){
 
 	var errorPanel  = document.getElementById("cwt_errorPanel");
-  var header      = document.getElementById("cwt_errorPanel_reason");
-  var stack       = document.getElementById("cwt_errorPanel_data");
+        var header      = document.getElementById("cwt_errorPanel_reason");
+        var stack       = document.getElementById("cwt_errorPanel_data");
 
 	controller.errorButtons = controller.generateButtonGroup( 
 	 	errorPanel,
@@ -11,17 +11,25 @@ util.scoped(function(){
 	    "cwt_panel_header_small cwt_page_button cwt_panel_button button_inactive"
 	);
 
+        // True if the error panel is visible, else false. +
+        //
+        // *Read-Only Property*
+        // 
 	controller.errorPanelVisible = false;
 
-	controller.showErrorPanel = function(msg,stackData){
-    stack.innerHTML               = stackData;
-    header.innerHTML              = msg;
-    errorPanel.style.display      = "block";
-    controller.errorPanelVisible  = true;
+        // Shows the error panel by a given error message and type.
+        // 
+	controller.showErrorPanel = function(msg,type,stackData){
+	    stack.innerHTML               = msg;
+	    header.innerHTML              = type;
+	    errorPanel.style.display      = "block";
+	    controller.errorPanelVisible  = true;
 	};
 
+        // Hides the error panel.
+        // 
 	controller.hideErrorPanel = function(){
-	  errorPanel.style.display      = "none";
-    controller.errorPanelVisible  = false;
+	    errorPanel.style.display      = "none";
+    	    controller.errorPanelVisible  = false;
 	};
 });
