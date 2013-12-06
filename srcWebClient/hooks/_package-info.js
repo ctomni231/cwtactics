428bbcbe63e64c44da75a85f680d94d7035be7e0
BlackCat <blackcat.myako@gmail.com>
2013-12-06 10:34:21 +0100

    Create _package-info.js

---
+++ srcWebClient/hooks/_package-info.js
@@ -0,0 +1,4 @@
+//
+// This package contains all logic that reacts on engine events. Basically this package will be used to setup
+// animations and recalculate visible game information on the screen.
+//
