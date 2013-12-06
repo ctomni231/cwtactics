82866dffc90b1d1cbcec4802654ceda07e70ab42
BlackCat <blackcat.myako@gmail.com>
2013-12-06 10:32:35 +0100

    Update _package-info.js

--- srcWebClient/controller/_package-info.js
+++ srcWebClient/controller/_package-info.js
@@ -1,5 +1,4 @@
 // 
-// 
-// 
-// 
+// This package contains several modules for the web client. The primary task of them is to control the
+// view and the input. All kind of logic that interacts with the client only stuff will be stored here.
 // 
