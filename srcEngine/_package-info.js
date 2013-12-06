d9f005d242b01d4f7cef4755159c8a8da09d613d
BlackCat <blackcat.myako@gmail.com>
2013-12-06 12:23:28 +0100

    Create _package-info.js

---
+++ srcEngine/_package-info.js
@@ -0,0 +1,12 @@
+/*
+
+## Custom Wars: Tactics
+
+### Global Variables
+
+*Custom Wars: Tactics* uses `model`, `controller` and `util` as global variables. If you 
+add other third party libraries then you have to make sure that they not using the same
+global variables. 
+
+
+*/ 
