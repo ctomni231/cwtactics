1df4b906daef4d8ccfb8691ced3e5f67a19718cb
BlackCat <blackcat.myako@gmail.com>
2013-12-16 09:56:37 +0100

    Create docs.js

---
+++ content/docs.js
@@ -0,0 +1,19 @@
+PAGE_DATA.documentation_user = [
+
+  { name:"Manual", link:"https://code.google.com/p/cwtactics/wiki/GameManual" }
+];
+
+PAGE_DATA.documentation_modding = [
+
+  { name:"Scripting",   link:null },
+  { name:"Mapping",     link:null },
+  { name:"Modding",     link:null },
+  { name:"Limitations", link:null }
+];
+
+PAGE_DATA.documentation_dev = [
+
+  { name:"Engine Documentation", link:null},
+  { name:"Gameplay - Features",  link:"https://github.com/ctomni231/cwtactics/wiki/Game-Features"},
+  { name:"Regression-Test Page", link:"https://github.com/ctomni231/cwtactics/wiki/Regression-Test" }
+];
