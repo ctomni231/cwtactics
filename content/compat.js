eacd180b8373f5671a12cb01831e0dab563d566f
BlackCat <blackcat.myako@gmail.com>
2013-12-16 10:07:57 +0100

    Create compat.js

---
+++ content/compat.js
@@ -0,0 +1,38 @@
+PAGE_DATA.compat = [
+
+  { name:"Mobile Safari", data:[
+    { version:"ios5", stat:2, info:"No Music" },
+    { version:"ios6", stat:3 },
+    { version:"ios7", stat:3 }
+  ]},
+  
+  { name:"Google Chrome", data:[
+    { version:"30", stat:3 }
+  ]},
+
+];
+
+PAGE_DATA.compat_link = null;
+
+PAGE_DATA.compat_link_header = "Compatibility Report";
+
+PAGE_DATA.compat_link_body   = "Hi I want to give you my compatibility report."+
+"\n "+
+"\n Date: "+
+"\n Used the \"Play CW:T\" button: Yes/No"+
+"\n "+
+"\n Browser: "+
+"\n Browser-Version: "+
+"\n OS: "+
+"\n OS-Version: "+
+"\n "+
+"\n Found Bugs:"+
+"\n   1) Situation:"+
+"\n      Expected:"+
+"\n   1) Situation:"+
+"\n "+
+"\n Do you cleared your cache?: Yes/No"+
+"\n "+
+"\n Do you cleared your cache?: Yes/No"+
+"\n "+
+"\n Greets"+;
