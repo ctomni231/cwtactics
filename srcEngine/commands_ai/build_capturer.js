84e12d09cbebaf44e3a90fc58968211042af984a
Tapsi <blackcat.myako@gmail.com>
2013-12-17 22:12:37 +0100

    surprise #2

--- srcEngine/commands_ai/build_capturer.js
+++ srcEngine/commands_ai/build_capturer.js
@@ -20,6 +20,65 @@ util.scoped(function(){
   //
   //
   controller.ai_defineRoutine({
+    key        : "buildUnits",
+    propAction : true,
+
+    scoring : function( data ){
+      var prid = data.source.propertyId;
+
+      if( data.source.unit ) return -1;
+
+      // isn't a factory
+      if( !model.factory_isFactory(prid)) return -1;
+
+      // there aren't any unit slots left or the man power is zero
+      if( !model.factory_canProduceSomething(prid,model.property_data[prid].owner) ){
+        if( DEBUG ) util.log("cannot build capturers because no slots left or no man power left");
+      }
+
+      menu.clear();
+      model.factoryGenerateBuildMenu(prid, menu);
+
+      // stuff buildable ?
+      if( menu.size === 0 ) return -1;
+
+      // search capturers
+      var gold = model.player_data[data.source.property.owner].gold;
+      var rand = parseInt(Math.random()*menu.size*2,10);
+      var oldRand = rand;
+      while(true){
+        for( var i=0, e=menu.size; i<e; i++ ){
+          var type = model.data_unitSheets[menu.data[i]];
+          if( type.cost <= gold ){
+            rand--;
+            if( rand < 0 ){
+              data.action.selectedSubEntry = type.ID;
+              return 20;
+            }
+          }
+        }
+
+        // not enough money for something...
+        if( rand === oldRand ) return -1;
+
+      }
+
+      return -1;
+    },
+
+    prepare : function( data ){
+      controller.action_objects.buildUnit.invoke(data);
+    }
+  });
+
+
+
+/*
+
+
+  //
+  //
+  controller.ai_defineRoutine({
     key        : "buildCapturers",
     propAction : true,
 
@@ -53,9 +112,9 @@ util.scoped(function(){
           data.action.selectedSubEntry = type.ID;
 
           // some scoring ideas:
-          //  a) score */ number of neutral properties
-          //  b) score */ number of enemy properties in range X
-          //  c) score */ distance to nearest (enemy) property
+          //  a) score / number of neutral properties
+          //  b) score / number of enemy properties in range X
+          //  c) score / distance to nearest (enemy) property
 
           return controller.ai_SCORE.HIGH;
         }
@@ -68,5 +127,7 @@ util.scoped(function(){
       controller.action_objects.buildUnit.invoke(data);
     }
   });
+
+*/
 
 });
