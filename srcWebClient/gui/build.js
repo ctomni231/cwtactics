7f389db0ae17c502e8754d8d59b60acf3b285ace
ctomni231 <ctomni231@gmail.com>
2013-12-22 09:26:00 -0800

    Quick Fix (Power of Money)
    Fixes #327. This changes all references of the "$" to the "G" symbol...
    at least for the build menu.

--- srcWebClient/gui/build.js
+++ srcWebClient/gui/build.js
@@ -2,6 +2,6 @@ controller.registerMenuRenderer("buildUnit",
 function( content, entry, index, enabled ){
   
   var cost = model.data_unitSheets[ content ].cost;
-  if( enabled ) entry.innerHTML = model.data_localized(content)+" ("+cost+"$)";
-  else entry.innerHTML = "<span style='color:red;'>"+model.data_localized(content)+" ("+cost+"$) </span>";
+  if( enabled ) entry.innerHTML = model.data_localized(content)+" ("+cost+" G)";
+  else entry.innerHTML = "<span style='color:red;'>"+model.data_localized(content)+" ("+cost+" G) </span>";
 });
