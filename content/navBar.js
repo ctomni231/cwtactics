05dc27f15fb16c2a33b1c5efb31af47f9fe9f7db
JakeSamiRulz <ctomni231@gmail.com>
2013-12-24 00:01:41 -0800

    Update navBar.js
    Ready, set, go

--- content/navBar.js
+++ content/navBar.js
@@ -1,9 +1,6 @@
 PAGE_DATA.navbarLinks = [
 
-  { name:"Play CW:T", 
-    /*
-section:"http://ctomni231.github.io/cwtactics/0_3_5/game/game.html" },
-    */   section:"http://battle.customwars.com/cwpages/games/milestones/m2.69/starter.html" },
+  { name:"Play CW:T", section:"http://ctomni231.github.io/cwtactics/0_3_5/game/game.html" },
   { name:"Recent News",   section:"#sectionNews" },
   { name:"Compatibility", section:"#sectionCompatibility" },
   { name:"Documentation", section:"#sectionManual" },
