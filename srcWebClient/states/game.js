aaabd9fd032906d2c9108668064dca406e3efaab
alex <alex@alexs-MacBook.local>
2014-01-10 23:20:16 +0100

    minimap fix

--- srcWebClient/states/game.js
+++ srcWebClient/states/game.js
@@ -185,30 +185,32 @@ util.scoped(function(){
   };
 
   controller.screenStateMachine.structure.GAMEROUND.CANCEL = function( ev,x,y ){
-    var mx,my;
-    if( arguments.length === 1 ){
-       mx = controller.mapCursorX;
-       my = controller.mapCursorY;
-    }else{
-       mx = x;
-       my = y;
-    }
+    if( !controller.menuVisible ){
+      var mx,my;
+      if( arguments.length === 1 ){
+         mx = controller.mapCursorX;
+         my = controller.mapCursorY;
+      }else{
+         mx = x;
+         my = y;
+      }
 
-    // if minimap is open, then close it
-    if( miniMapOpen ){
-      controller.minimap_hideIngameMinimap();
-      miniMapOpen = false;  
-      return this.breakTransition();
-    }
+      // if minimap is open, then close it
+      if( miniMapOpen ){
+        controller.minimap_hideIngameMinimap();
+        miniMapOpen = false;  
+        return this.breakTransition();
+      }
 
-    var prop = model.property_posMap[mx][my];
-    var unit = model.unit_posData[mx][my];
-    
-    // if minimap is not open and the click happens on an empty tile, then show it
-    if(  !model.fog_turnOwnerData[mx][my] || ( !prop && !unit )){
-      controller.minimap_showIngameMinimap();
-      miniMapOpen = true;   
-      return this.breakTransition();
+      var prop = model.property_posMap[mx][my];
+      var unit = model.unit_posData[mx][my];
+      
+      // if minimap is not open and the click happens on an empty tile, then show it
+      if(  !model.fog_turnOwnerData[mx][my] || ( !prop && !unit )){
+        controller.minimap_showIngameMinimap();
+        miniMapOpen = true;   
+        return this.breakTransition();
+      }
     }
 
     var state = controller.stateMachine.state;
