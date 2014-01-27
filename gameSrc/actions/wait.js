3747f13f07fcd1bc9c0b9a993a18b37c208fd8d5
BlackCat <blackcat.myako@gmail.com>
2014-01-27 13:26:08 +0100

    Update wait.js

--- gameSrc/actions/wait.js
+++ gameSrc/actions/wait.js
@@ -1,17 +1,17 @@
 new cwt.Action("wait")
-      .unitAction()
-      .noAutoWait() // wait is already the wait action :P
-      .relation([
+  .unitAction()
+  .noAutoWait() // wait is already the wait action :P
+  .relation([
         "S", "T",
         cwt.Player.RELATION_MODES.NONE,
         cwt.Player.RELATION_MODES.SAME_OBJECT
-      ])  
-      .condition(function (data) {
+  ])  
+  .condition(function (data) {
         return cwt.Gameround.canAct(data.source.unitId);
-      })  
-      .action(function (data) {
+  })
+  .action(function (data) {
         controller.commandStack_sharedInvokement(
           "wait_invoked",
           data.source.unitId
         );
-      });
+  });
