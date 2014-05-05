0d2b94ce35f08a997f33d1f00297a5d2f4a3dd07
ctomni231 <ctomni231@gmail.com>
2014-05-05 10:28:41 -0700

    JavaScript Update (Weathervane)
    Redid the snow to render by caching the item. It is much faster as a
    result of the cache.

--- gameSrc/test/weather.js
+++ gameSrc/test/weather.js
@@ -19,14 +19,8 @@ cwt.Gameflow.addState({
 	//But it seems memory intensive to pull off, there has to be
 	//a less expensive way
 	
-	//Keeps track of the total time
-	this.TIME = 1;
-	
 	//Keeps track of the frequency of a snowball
 	this.FREQUENCY = 2;
-	
-	//Keeps track of the delta (real) time
-	this.time = 0;
 	
 	//The type of snowball to draw
 	this.type = [];
@@ -34,6 +28,19 @@ cwt.Gameflow.addState({
 	this.posx = [];
 	//The y-axis position of the snowball
 	this.posy = [];
+	
+	//Holds the graphics for a simple snowball (cache)
+	//Totally hard coded all the values here
+	this.ball = document.createElement('canvas');
+	this.ball.width = 10;
+	this.ball.height = 10;
+	
+	var ctx = this.ball.getContext('2d');
+	ctx.strokeStyle = "rgba(255,255,255,0.3)";
+	ctx.lineWidth = 4;
+	ctx.beginPath();
+	ctx.arc(5, 5, 2, 0, Math.PI*2);
+	ctx.stroke();
   },
 
   enter: function () {
@@ -46,48 +53,41 @@ cwt.Gameflow.addState({
 	// action leads into main menu (Thanks BlackCat
     if (lastInput && lastInput.key === cwt.Input.TYPE_ACTION)
       cwt.Gameflow.changeState("MAIN_MENU");
-	  
-	this.time += delta;
-	if (this.time > this.TIME) {
 		
-		//Particle creation (totally fixed up to take as little memory as possible)
-		for(var i = 0; i < this.type.length+1; i++){
-			if(parseInt(Math.random()*this.FREQUENCY) == 1){
-				if(i == this.type.length){
-					this.type.push(parseInt(Math.random()*3));
-					this.posx.push(((Math.random()*cwt.Screen.width+100/10)*10)-200);
-					this.posy.push(-10);
-					break;
-				}
-				if(this.type[i] == -1){
-					this.type[i] = parseInt(Math.random()*3);
-					this.posx[i] = ((Math.random()*cwt.Screen.width+100/10)*10)-200;
-					this.posy[i] = -10;
-					break;
-				}
-				
+	//Particle creation (totally fixed up to take as little memory as possible)
+	for(var i = 0; i < this.type.length+1; i++){
+		if(parseInt(Math.random()*this.FREQUENCY) == 1){
+			if(i == this.type.length){
+				this.type.push(parseInt(Math.random()*3));
+				this.posx.push(((Math.random()*cwt.Screen.width+100/10)*10)-200);
+				this.posy.push(-10);
+				break;
 			}
+			if(this.type[i] == -1){
+				this.type[i] = parseInt(Math.random()*3);
+				this.posx[i] = ((Math.random()*cwt.Screen.width+100/10)*10)-200;
+				this.posy[i] = -10;
+				break;
+			}
+			
 		}
+	}
 		
-		//Snow particle updates
-		for(var i = 0; i < this.type.length; i++){
-			if(this.type[i] == -1)
-				continue;
+	//Snow particle updates
+	for(var i = 0; i < this.type.length; i++){
+		if(this.type[i] == -1)
+			continue;
 
-			if(this.type[i] == 2){
-				this.posx[i] += 1;
-			}
+		if(this.type[i] == 2){
 			this.posx[i] += 1;
-			this.posy[i] += 4;
-
-			//Destroy particles
-			if(this.posy[i] > cwt.Screen.height+100)
-				this.type[i] = -1;
 		}
+		this.posx[i] += 1;
+		this.posy[i] += 4;
 
-		//updates time
-        this.time -= this.TIME;
-	};
+		//Destroy particles
+		if(this.posy[i] > cwt.Screen.height+100)
+			this.type[i] = -1;
+	}
   },
 
   render: function () {
@@ -101,13 +101,14 @@ cwt.Gameflow.addState({
 		if(this.type[i] == -1)
 			continue;
 				
+		ctx.drawImage(this.ball, this.posx[i], this.posy[i], 10+5*this.type[i], 10+5*this.type[i]);
 		//arc(x, y, radius, startAngle, endAngle, counterClockwise(opt) )
 		//Ugh, so hard to draw circles (Played with line width until correct
-		ctx.strokeStyle = "rgba(255,255,255,0.3)";
-		ctx.lineWidth = 4 + this.type[i]*2;
-		ctx.beginPath();
-		ctx.arc(this.posx[i], this.posy[i], 2 + this.type[i], 0, Math.PI*2);
-		ctx.stroke();
+		//ctx.strokeStyle = "rgba(255,255,255,0.3)";
+		//ctx.lineWidth = 4 + this.type[i]*2;
+		//ctx.beginPath();
+		//ctx.arc(this.posx[i], this.posy[i], 2 + this.type[i], 0, Math.PI*2);
+		//ctx.stroke();
 	}
   }
 });
