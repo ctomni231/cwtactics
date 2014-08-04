7f8ab392c834eda12cb10b5998f66bc53b703125
BlackCat <blackcat.myako@gmail.com>
2014-08-04 15:13:48 +0200

    Update MAX.js

--- modifications/cwt/cos/MAX.js
+++ modifications/cwt/cos/MAX.js
@@ -4,46 +4,45 @@ require('../../sheets').commanders.registerSheet({
   "faction"  : "BLMN",
   "music"    : "music/Max.mp3",
   "coStars"  : 3,
-  "scoStars" : 3
-
-  /*
-  "d2d"      : [
-    {
-      "$when":[
-        "ATTACK_TYPE",["DIRECT"]
-      ],
-      "att":20
+  "scoStars" : 3,
+  
+	"effects": {
+	  
+	  "D2D":{
+	    "att": [
+	      "if( direct(unit),",
+					"if(not(infantry(unit)),",
+						"if(gameMode(AW2),",
+							"+(50,value),",
+							"+(20,value))),",
+					"-(10,value))"
+			],
+	    "maxrange": "if( indirect(unit), -(1,value), value)"
+	  },
+	  
+    "COP":{
+	    "att": [
+	      "if( direct(unit),",
+					"if(not(infantry(unit)),",
+						"+(40,value), ",
+						"value),",
+					"value)"
+			],
+    	"def":				"+(10,value)"
+    	"movepoints":	"if( gameMode(AW2), +(1,value), value)"
     },
-
-    {
-      "$when":[
-        "ATTACK_TYPE",["INDIRECT"]
-      ],
-      "maxrange":-1
+    
+    "SCOP":{
+	    "att": [
+	      "if( direct(unit),",
+					"if(not(infantry(unit)),",
+						"+(70,value), ",
+						"value),",
+					"value)"
+			],
+    	"def":				"+(10,value)"
+    	"movepoints":	"if( gameMode(AW2), +(2,value), value)"
     }
-  ],
-  "cop" : {
-    "turn":[
-      {
-        "$when":[
-          "ATTACK_TYPE",["DIRECT"]
-        ],
-        "att":40
-      }
-    ],
-    "power":{}
-  },
-  "scop" : {
-    "turn":[
-      {
-        "$when":[
-          "ATTACK_TYPE",["DIRECT"]
-        ],
-        "att":70
-      }
-    ],
-    "power":{}
   }
-  */
 });
 
