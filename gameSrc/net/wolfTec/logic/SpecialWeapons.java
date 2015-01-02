b8604c453ec3953adfc9c0452a1559f269b7811d
BlackCat <blackcat.myako@gmail.com>
2015-01-03 00:18:13 +0100

    Logic package

---
+++ gameSrc/net/wolfTec/logic/SpecialWeapons.java
@@ -0,0 +1,45 @@
+package net.wolfTec.logic;
+
+import net.wolfTec.CustomWarsTactics;
+import net.wolfTec.model.Property;
+import net.wolfTec.model.Unit;
+
+import org.stjs.javascript.JSGlobal;
+import org.stjs.javascript.annotation.Namespace;
+
+@Namespace("cwt") public class SpecialWeapons {
+
+	public static boolean	$BEAN	= true;
+
+	/**
+	 * Returns **true** when the given **unit** is the mechanical laser trigger,
+	 * else **false**.
+	 *
+	 * @return
+	 */
+	public boolean isLaser(Unit unit) {
+		return (unit.getType().ID == CustomWarsTactics.LASER_UNIT_INV);
+	}
+
+	/**
+	 * Returns **true** if a given **unit** is a cannon trigger unit, else
+	 * **false**.
+	 *
+	 * @return
+	 */
+	public boolean isCannonUnit(Unit unit) {
+		return (unit.getType().ID == CustomWarsTactics.CANNON_UNIT_INV);
+	}
+
+	/**
+	 * Returns true if a property id is a rocket silo. A rocket silo has the
+	 * ability to fire a rocket to a position with an impact.
+	 *
+	 * @return
+	 */
+	public boolean isRocketSilo(Property prop) {
+		return (prop.type.rocketsilo != JSGlobal.undefined); // TODO: null replace
+																													// possible
+	}
+
+}
---
+++ gameSrc/net/wolfTec/logic/Transport.java
@@ -0,0 +1,33 @@
+package net.wolfTec.logic;
+
+import org.stjs.javascript.annotation.Namespace;
+
+import net.wolfTec.Constants;
+import net.wolfTec.model.GameRound;
+import net.wolfTec.model.Unit;
+
+@Namespace("cwt") public class Transport {
+
+	public static boolean	$BEAN	= true;
+	private GameRound			$gameround;
+
+	/**
+	 * @return true if the unit with id tid is a transporter, else false.
+	 */
+	public boolean isTransportUnit(Unit unit) {
+		return (unit.getType().maxloads > 0);
+	}
+
+	/**
+	 * Has a transporter unit with id tid loaded units?
+	 * 
+	 * @return {boolean} true if yes, else false.
+	 */
+	public boolean hasLoads(Unit unit) {
+		for (int i = 0, e = Constants.MAX_UNITS; i < e; i++) {
+			if (unit == $gameround.getUnit(i).getLoadedIn()) return true;
+		}
+		return false;
+	}
+
+}
