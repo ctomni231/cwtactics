9668903be8b7517f996482f0f74ee3df839c9a92
BlackCat <blackcat.myako@gmail.com>
2015-01-20 07:57:23 +0100

    Added DI and IOC to WTEngine

--- engineSrc/net/wolfTec/wtEngine/base/WolfTecEngine.java
+++ engineSrc/net/wolfTec/wtEngine/base/WolfTecEngine.java
@@ -1,11 +1,15 @@
 package net.wolfTec.wtEngine.base;
 
 import org.stjs.javascript.annotation.Namespace;
+import net.wolfTec.wtEngine.log.LoggerFactoryBeanInterface;
 
 @Namespace("wtEngine") public class WolfTecEngine {
-
+  
+	private static Map<String, Object>	beans;
+	
   public WolfTecEngine(EngineOptions options) {
-    
+    initBeans();
+    solveBeanDependencies();
   }
 
   public <T> T getBean (String bean) {
@@ -27,4 +31,62 @@ import org.stjs.javascript.annotation.Namespace;
   public String getLongName () {
     return "WolfTecEngine © BlackCat and JSRulez";
   }
+  
+  /**
+	 * <strong>Note: </strong> This function is low level and contains real JS
+	 * code. Modify only if you know what you're doing here.
+	 */
+	private static void initBeans() {
+		beans = JSCollections.$map();
+
+		// search in all classes and convert every class with a name that ends with the string "Bean" 
+		// into a bean by calling it's constructor with zero arguments.
+		Array<String> possibleBeanNames = JSObjectAdapter.$js("Object.keys(cwt)");
+		for (String name : possibleBeanNames) {
+			if (name.endsWith("Bean")) {
+				JSObjectAdapter.$js("this.beans[name] = new cwt[name]()");
+			}
+		}
+	}
+
+	/**
+	 * <strong>Note: </strong> This function is low level and contains real JS
+	 * code. Modify only if you know what you're doing here.
+	 */
+	private static void solveBeanDependencies() {
+		boolean isDebugEnabled = Constants.DEBUG;
+		LoggerFactoryBeanInterface logFactory = (LoggerFactoryBeanInterface) beans.get("LoggerFactoryBean");
+		
+		// search in all beans for properties with a leading '$' character. This
+		// properties are references to beans. Place the right bean into this
+		// property by searching the correct bean type together with the type
+		// description of the class.
+		Array<String> beanNames = JSObjectAdapter.$js("Object.keys(this.beans)");
+		for (String beanName : beanNames) {
+      
+      Object bean = beans.$get(beanName);
+			Array<String> beanProperties = JSObjectAdapter.$js("Object.keys(bean)");
+			for (String property : beanProperties) {
+				
+				boolean hasTypeDescription = JSObjectAdapter.$js("bean.constructor.$typeDescription.hasOwnProperty(property)");
+				if (hasTypeDescription) {
+				  
+					// extract full class name (including namespace) from type description
+					String propertyClass = JSObjectAdapter.$js("bean.constructor.$typeDescription[property]");
+					
+					if (propertyClass == "Logger") {
+						JSObjectAdapter.$js("bean[property] = logFactory.getLogger(beanName, isDebugEnabled)");
+
+					} else if (propertyClass.endsWith("Bean")) {
+					  // remove namespace from propertyClass 
+					  propertyClass = propertyClass.substring(propertyClass.lastIndexOf(".") + 1);
+					  JSObjectAdapter.$js("bean[property] = new this.beans[propertyClass]()");
+							
+					} else {
+					  // property with type, but no reference to a bean
+					}
+				}
+			}
+		}
+	}
 }
