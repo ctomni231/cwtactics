2b6b237e376c32bf650dc98de5e7ff7af3eabc03
BlackCat <blackcat.myako@gmail.com>
2015-01-20 07:54:28 +0100

    Update LoggerFactoryBeanInterface.java

--- engineSrc/net/wolfTec/wtEngine/log/LoggerFactoryBeanInterface.java
+++ engineSrc/net/wolfTec/wtEngine/log/LoggerFactoryBeanInterface.java
@@ -3,5 +3,5 @@ package net.wolfTec.wtEngine.log;
 import org.stjs.javascript.annotation.Namespace;
 
 @Namespace("wtEngine") public interface LoggerFactoryBeanInterface {
-  public Logger getLogger(String name);
+  public Logger getLogger(String name, boolean isDebugEnabled);
 }
