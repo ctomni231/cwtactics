2cfe1cf953e6977df8d30174a35ae85c54ab091f
BlackCat <blackcat.myako@gmail.com>
2015-02-09 19:51:05 +0100

    #379

--- utilitySrc/org/wolfTec/cwt/utility/gherkin/GherkinTestRunner.java
+++ utilitySrc/org/wolfTec/cwt/utility/gherkin/GherkinTestRunner.java
@@ -1,22 +1,13 @@
 package org.wolfTec.cwt.utility.gherkin;
 
-import org.wolfTec.cwt.game.log.Logger;
-import org.wolfTec.cwt.utility.beans.Bean;
-import org.wolfTec.cwt.utility.beans.Injected;
 
-@Bean public class GherkinTestRunner {
-  
-  @Injected private Logger log;
+public class GherkinTestRunner {
   
   public void runAll () {
     
   }
   
   public void runTest () {
-    log.info("STARTED");
-
-    log.info("PASSED");
-    log.error("FAILED");
     
   }
 }
