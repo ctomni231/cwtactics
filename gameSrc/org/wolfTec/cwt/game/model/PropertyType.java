69948a31a32fcaf797eba35a5e4f6e5f950d3c48
BlackCat <blackcat.myako@gmail.com>
2015-02-10 22:58:34 +0100

    #375

--- gameSrc/org/wolfTec/cwt/game/model/PropertyType.java
+++ gameSrc/org/wolfTec/cwt/game/model/PropertyType.java
@@ -3,6 +3,7 @@ package org.wolfTec.cwt.game.model;
 import org.stjs.javascript.Map;
 import org.wolfTec.cwt.game.Constants;
 import org.wolfTec.cwt.utility.validation.IntValue;
+import org.wolfTec.cwt.utility.validation.StringKey;
 import org.wolfTec.cwt.utility.validation.StringValue;
 
 public class PropertyType extends ObjectType {
@@ -15,7 +16,10 @@ public class PropertyType extends ObjectType {
   public Object builds;
   public LaserType laser;
   @StringValue(minLength = 1) public String changesTo;
+  
+  @StringKey(minLength = 4, maxLength = 4) 
   @IntValue(min = 1, max = 10) public Map<String, Integer> repairs;
+  
   @IntValue(min = 0, max = 99999) public int funds = 0;
   public boolean looseAfterCaptured = false;
   public boolean blocker = false;
