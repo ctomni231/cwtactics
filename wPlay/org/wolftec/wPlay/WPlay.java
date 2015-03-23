14bd2a910387e3e698b0c2d4f5a8786e10d37a72
tapsi <blackcat.myako@gmail.com>
2015-03-23 21:32:51 +0100

    #379

---
+++ wPlay/org/wolftec/wPlay/WPlay.java
@@ -0,0 +1,8 @@
+package org.wolftec.wPlay;
+
+public abstract class WPlay {
+
+  public static String getVersion() {
+    return "1.0.0";
+  }
+}
---
+++ wolftecCoreSrc/org/wolftec/WCore.java
@@ -0,0 +1,8 @@
+package org.wolftec;
+
+public abstract class WCore {
+
+  public static String getVersion() {
+    return "1.0.0";
+  }
+}
