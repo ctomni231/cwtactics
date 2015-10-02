1a2863ab450ecb3f5f27d6e6dc1a7f1180a68903
ctomni231 <ctomni231@gmail.com>
2015-10-01 23:22:52 -0700

    CWT Commit - COIcons and JSlix
    Did some more coding research on JSlix Image Library, and have all the
    tools to make it an eventual effective system. Also put in some new
    assets from secv90 regarding the Commanding Officer Icons. Thanks again
    to secv90 for his continuous spriting work on the project.

--- jstest/ImageLibrary.js
+++ jstest/ImageLibrary.js
@@ -3,6 +3,8 @@
 
 var x = 0;
 var y = 0;
+var counter = 0;
+var text = "CWT_MECH.png";
 
 //The images have to be loaded within the function. I guess they can't load in time...
 //Let me try putting the images in head...
@@ -18,6 +20,10 @@ function copy(){
 				
 	var unit = document.getElementById("unit");
 	ctx.drawImage(unit, 130, 30+y);
+	
+	var unit = document.getElementById("image");
+	if(unit != null)
+		ctx.drawImage(unit, 60, 30+y);
 				
 	y+= 10;
 }
@@ -25,3 +31,42 @@ function copy(){
 // The top will deprecate as I figure out more about the system
 // Now I need to play around with getting the pixels
 
+function store(){
+
+	//This grabs an image and temporarily stores it in memory
+	var imgStorage = document.getElementById("image");
+	if(imgStorage == null){
+		imgStorage = document.createElement("img");
+		document.body.appendChild(imgStorage);
+	}
+	imgStorage.setAttribute("id", "image");
+	imgStorage.setAttribute("src", text);
+	imgStorage.setAttribute("style", "display:none");
+	
+	//This makes a canvas storage module for the image
+	var canvas = document.getElementById("store");
+	if(canvas == null){
+		canvas = document.createElement("canvas");
+		document.body.appendChild(canvas);
+	}
+	canvas.setAttribute("id", "store");
+	canvas.setAttribute("width", imgStorage.width);
+	canvas.setAttribute("height", imgStorage.height);
+	canvas.setAttribute("style", "display:none");
+	
+	//Attempt to draw something in the newly created canvas
+	var ctx = canvas.getContext("2d");
+	ctx.drawImage(image, 0, 0);
+	
+	//Get the pixels from the image
+	var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	for(var i = 0; i < imgData.data.length; i+=4){
+	}
+	
+	//After we get all the pixels, then we have to create the buffer to store each pixel into.
+	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Typed_arrays
+	//The type we are trying to get is...
+	
+	// var buffer = new ArrayBuffer(a); (Where "a" is the total number of bytes in the buffer)	
+	// var Uint8View = new Uint8Array(buffer);
+}
