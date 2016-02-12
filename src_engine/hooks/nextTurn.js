1097529ddd2aa9e7028efc3f6a33d39c2953b12d
BlackCat <blackcat.myako@gmail.com>
2016-02-12 21:06:26 +0100

    fixes "round x - player null" bug

--- src_engine/hooks/nextTurn.js
+++ src_engine/hooks/nextTurn.js
@@ -1,50 +1,44 @@
-util.scoped(function(){
+util.scoped(function () {
 
   /* MOVE INTO */
 
-    function playAudio( _, id ){
-      controller.audio_playMusic(id);
-    }
+  function playAudio(_, id) {
+    controller.audio_playMusic(id);
+  }
 
-    function storeAudio( obj ){
-      var audioData = Base64Helper.decodeBuffer( obj.value );
-      controller.audio_loadByArrayBuffer( obj.key, audioData, playAudio );
-    }
+  function storeAudio(obj) {
+    var audioData = Base64Helper.decodeBuffer(obj.value);
+    controller.audio_loadByArrayBuffer(obj.key, audioData, playAudio);
+  }
 
-    function loadAudio( key ){
-      controller.storage_assets.get( key, storeAudio );
-    }
+  function loadAudio(key) {
+    controller.storage_assets.get(key, storeAudio);
+  }
 
   /* END - MOVE INTO AUDIO */
 
   view.registerAnimationHook({
     key: "nextTurn_invoked",
 
-    prepare: function(){
+    prepare: function () {
 
       var co = model.co_data[model.round_turnOwner].coA;
-      if( co ){
-        if( !controller.audio_isBuffered( co.music ) ){
+      if (co) {
+        if (!controller.audio_isBuffered(co.music)) {
           loadAudio(co.music);
         } else {
-          playAudio(false,co.music);
+          playAudio(false, co.music);
         }
       }
-
-      view.showInfoMessage(
-        model.data_localized("day")+
-          " "+
-          model.round_day+
-          " - "+
-          model.player_data[model.round_turnOwner].name
-      );
+      var playerName = model.player_data[model.round_turnOwner].name || "Player " + model.round_turnOwner;
+      view.showInfoMessage(model.data_localized("day") + " " + model.round_day + " - " + playerName);
     },
 
-    render: function(){},
+    render: function () {},
 
-    update: function( delta ){},
+    update: function (delta) {},
 
-    isDone: function(){
+    isDone: function () {
       return !view.hasInfoMessage();
     }
 
