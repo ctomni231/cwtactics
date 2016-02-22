c798099ce89a5e4df27cf8d6c0034deb3d05e8d7
BlackCat <blackcat.myako@gmail.com>
2016-02-22 10:04:08 +0100

    #425

--- src_engine/core/storage.js
+++ src_engine/core/storage.js
@@ -2,7 +2,7 @@
 
 cwt.storage_save = function (key, item, when_done, error_receiver) {
   localforage.setItem(key, item, function (err, value) {
-    if (err === null) {
+    if (err !== null) {
       error_receiver(err);
     } else {
       when_done();
@@ -12,7 +12,7 @@ cwt.storage_save = function (key, item, when_done, error_receiver) {
 
 cwt.storage_load = function (key, item_receiver, error_receiver) {
   localforage.getItem(key, function (err, value) {
-    if (err === null) {
+    if (err !== null) {
       error_receiver(err);
     } else {
       item_receiver(value);
@@ -22,7 +22,7 @@ cwt.storage_load = function (key, item_receiver, error_receiver) {
 
 cwt.storage_remove_item = function (key, when_done, error_receiver) {
   localforage.removeItem(key, function (err) {
-    if (err === null) {
+    if (err !== null) {
       error_receiver(err);
     } else {
       when_done();
@@ -32,7 +32,7 @@ cwt.storage_remove_item = function (key, when_done, error_receiver) {
 
 cwt.storage_remove_all = function (when_done, error_receiver) {
   localforage.clear(function (err) {
-    if (err === null) {
+    if (err !== null) {
       error_receiver(err);
     } else {
       when_done();
@@ -42,7 +42,7 @@ cwt.storage_remove_all = function (when_done, error_receiver) {
 
 cwt.storage_for_each_key = function (keys_receiver, error_receiver) {
   localforage.keys(function (error, keys) {
-    if (error === null) {
+    if (error !== null) {
       error_receiver(error);
     } else {
       keys_receiver(keys);
