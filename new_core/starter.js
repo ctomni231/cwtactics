09202cfd9795fbc3de3883f7604d158257c0af84
blackcat <blackcat.myako@gmail.com>
2019-01-20 00:15:36 +0100

    feature: test module

--- new_core/starter.js
+++ new_core/starter.js
@@ -1,6 +1,10 @@
 import { startLoop } from "./loop_manager.js"
 import { loop } from "./gameloop.js"
+import { state } from "./state.js"
 
 export function boot () {
+
+  state.current = "INITIAL"
+
   startLoop(loop)
 }
--- new_core/statemachine.js
+++ new_core/statemachine.js
@@ -1,4 +1,5 @@
 import * as gamedata from "./state.js"
+import { moduleTest } from "./test.js"
 
 export function update () {
   const nextState = gamedata.state.next
@@ -8,3 +9,24 @@ export function update () {
     gamedata.state.next = null
   }
 }
+
+moduleTest("statemachine", (testCase) => {
+
+  testCase("should change state when next state is set", (assertThat) => {
+    gamedata.state.current = "A"
+    gamedata.state.next = "B"
+
+    update()
+
+    assertThat(gamedata.state.current).is("B")
+  })
+
+  testCase("should not change state when no next state is set", (assertThat) => {
+    gamedata.state.current = "A"
+    gamedata.state.next = null
+
+    update()
+
+    assertThat(gamedata.state.current).is("A")
+  })
+})
