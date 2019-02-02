7532d21c1caf1728a8af743702fa7e19ede962b7
blackcat <blackcat.myako@gmail.com>
2019-02-02 23:42:48 +0100

    bigger canvas scale

--- new_core/config/debugPages.js
+++ new_core/config/debugPages.js
@@ -9,24 +9,24 @@ export const MAIN_INFO = {
     },
     row4: {
       col1: [ "Unstable", "performance.gameloop.unstable" ],
-      col2: [ "Avg Time", "performance.gameloop.averageDuration" ],
-      col3: [ "Last Time", "performance.gameloop.lastDuration" ]
+      col2: [ "avg", "performance.gameloop.averageDuration" ],
+      col3: [ "current", "performance.gameloop.lastDuration" ]
     },
     row6: {
       col1: [ "-- Update --", "" ],
     },
     row7: {
       col1: [ "Unstable", "performance.screen_update.unstable" ],
-      col2: [ "Avg Time", "performance.screen_update.averageDuration" ],
-      col3: [ "Last Time", "performance.screen_update.lastDuration" ]
+      col2: [ "avg", "performance.screen_update.averageDuration" ],
+      col3: [ "current", "performance.screen_update.lastDuration" ]
     },
     row9: {
       col1: [ "-- Render --", "" ],
     },
     row10: {
       col1: [ "Unstable", "performance.screen_render.unstable" ],
-      col2: [ "Avg Time", "performance.screen_render.averageDuration" ],
-      col3: [ "Last Time", "performance.screen_render.lastDuration" ]
+      col2: [ "avg", "performance.screen_render.averageDuration" ],
+      col3: [ "current", "performance.screen_render.lastDuration" ]
     }
   }
 }
@@ -35,8 +35,19 @@ export const MAP_INFO = {
   name: "Map",
   data: {
     row1: {
-      col1: [ "Width", "map.width" ],
-      col2: [ "Height", "map.height" ]
+      col1: [ "Size", "" ],
+      col2: [ "width", "map.width" ],
+      col3: [ "height", "map.height" ]
+    },
+    row2: {
+      col1: [ "Cursor", "" ],
+      col2: [ "mapX", "cursor.map.x" ],
+      col3: [ "mapY", "cursor.map.y" ]
+    },
+    row3: {
+      col1: [ "", "" ],
+      col2: [ "screenX", "cursor.screen.x" ],
+      col3: [ "screenY", "cursor.screen.y" ]
     }
   }
 }
